29e111f514b692874ab13a4adb836a426119731e
Param Pragyan <parampragyan@gmail.com>
2024-12-04 19:33:40 +0530

    minor changes

--- backend/router.js
+++ backend/router.js
@@ -3,7 +3,7 @@ const videoController = require('./controller');
 
 const router = express.Router();
 
-router.post('/upload', videoController.uploadVideo); 
+router.post('/', videoController.uploadVideo); 
 router.get('/videolist', videoController.getVideos); 
 router.get('/:title', videoController.getVideoByTitle); 
 
